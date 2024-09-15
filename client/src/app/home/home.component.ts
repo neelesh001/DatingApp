d9c92685a6d5a80c91b6291edb091552acf3045a
neelesh001 <neelesh001@gmail.com>
2024-09-15 11:03:53 +0530

    Section 7

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { RegisterComponent } from "../register/register.component";
+import { RegisterComponent } from '../register/register.component';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -7,9 +7,9 @@ import { HttpClient } from '@angular/common/http';
   standalone: true,
   imports: [RegisterComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
   http = inject(HttpClient);
   registerMode = false;
   users: any;
@@ -28,9 +28,9 @@ export class HomeComponent implements OnInit{
 
   getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')      
-    })
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Request has completed'),
+    });
   }
 }
