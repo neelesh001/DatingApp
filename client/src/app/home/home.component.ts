0da97c4279f0d6c25c29c6cfb85e46e497ddafbc
neelesh001 <neelesh001@gmail.com>
2024-09-20 07:58:27 +0530

    Section 9

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RegisterComponent } from '../register/register.component';
-import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
@@ -9,10 +9,8 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit {
-  http = inject(HttpClient);
+export class HomeComponent  {
   registerMode = false;
-  users: any;
 
   registerToggle() {
     this.registerMode = !this.registerMode;
@@ -21,16 +19,5 @@ export class HomeComponent implements OnInit {
   cancelRegisterMode(event: boolean) {
     this.registerMode = event;
   }
-
-  ngOnInit(): void {
-    this.getUsers();
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log(error),
-      complete: () => console.log('Request has completed'),
-    });
-  }
 }
+
